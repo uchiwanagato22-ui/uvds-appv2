8a7f0fdbf3fc653061d96c9edcea5e1113a0d9e5
uchiwanagato22 <uchiwanagato22@email.com>
2026-05-22 01:22:51 +0000

    fix: resolve admin and stats access compilation errors in all_screens

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,19 +1,12 @@
-import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+﻿import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) return web; // <--- AJOUTE CETTE LIGNE
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      default:
-        throw UnsupportedError('Plateforme non supportée');
-    }
+    if (kIsWeb) return web;
+    return android;
   }
 
-  // <--- RECOPIE CE BLOC DE CONFIGURATION WEB ICI :
   static const FirebaseOptions web = FirebaseOptions(
     apiKey: 'AIzaSyD_gmlLb6VGbuA8m93H6VjunMvbB-6fTHk',
     appId: '1:145848279394:web:cd71be4d80a373cf9cc3b',
@@ -21,3 +14,12 @@ class DefaultFirebaseOptions {
     projectId: 'uvds-c316e',
     storageBucket: 'uvds-c316e.firebasestorage.app',
   );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyD_gmlLb6VGbuA8m93H6VjunMvbB-6fTHk',
+    appId: '1:145848279394:android:8717ec68d67e2a72f9cc3b',
+    messagingSenderId: '145848279394',
+    projectId: 'uvds-c316e',
+    storageBucket: 'uvds-c316e.firebasestorage.app',
+  );
+}
